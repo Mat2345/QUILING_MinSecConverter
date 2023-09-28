5944e9b83bde79b54475f38342787ce4a378c245
Matthew Agustes G. Quiling <140613031+Mat2345@users.noreply.github.com>
2023-09-28 20:39:13 +0800

    Add files via upload

---
+++ QUILING_MinSecConverter.py
@@ -0,0 +1,7 @@
+total_seconds = int(input("Enter an integer for seconds:"))
+
+minutes = total_seconds //60
+second = total_seconds %60
+
+print(f"{total_seconds} second is equal to {minutes} minutes and {second} seconds. ")
+
